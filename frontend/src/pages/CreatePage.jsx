e1d69649554eff1535478d7908135fe73ffbb1c2
Malek Ben Salah <malekbensalah056@gmail.com>
2025-10-22 14:08:05 +0100

    fixing a silly mistake :/

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -46,7 +46,7 @@ const CreatePage = () => {
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto p-4 py-8">
         <div className="max-w-2xl mx-auto">
-          <Link to={"/HomePage"} className="btn btn-ghost mb-6">
+          <Link to={"/"} className="btn btn-ghost mb-6">
             <ArrowLeftIcon className="size-5" />
             Back to Notes
           </Link>
